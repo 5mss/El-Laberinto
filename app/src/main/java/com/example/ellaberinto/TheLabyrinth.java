0ca5ef1d7713a608a93c6d037ce75ea969bfa74c
吴浩渺 <wuhaomiao@wuhaomiaos-MacBook-Pro.local>
2019-10-17 17:49:10 +0800

    complete basic openGL settings

--- app/src/main/java/com/example/ellaberinto/TheLabyrinth.java
+++ app/src/main/java/com/example/ellaberinto/TheLabyrinth.java
@@ -3,10 +3,13 @@ package com.example.ellaberinto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.vr.ndk.base.Properties;
+import com.google.vr.sdk.base.AndroidCompat;
 import com.google.vr.sdk.base.Eye;
 import com.google.vr.sdk.base.GvrActivity;
 import com.google.vr.sdk.base.GvrView;
@@ -22,16 +25,80 @@ public class TheLabyrinth extends GvrActivity implements GvrView.StereoRenderer 
 
     private static final String TAG = "TheLabyrinth";
 
+    private int objectProgram;
+    private int objectPositionParam;
+    private int objectUvParam;
+    private int objectModelViewProjectionParam;
+
+    private static final String OBJECT_VERTEX_SHADER_CODE =
+                    "uniform mat4 u_MVP;"+
+                    "attribute vec4 a_Position;"+
+                    "attribute vec2 a_UV;"+
+                    "varying vec2 v_UV;"+
+                    ""+
+                    "void main() {"+
+                    "  v_UV = a_UV;"+
+                    "  gl_Position = u_MVP * a_Position;"+
+                    "}";
+
+    private static final String OBJECT_FRAGMENT_SHADER_CODE =
+                    "precision mediump float;"+
+                    "varying vec2 v_UV;"+
+                    "uniform sampler2D u_Texture;"+
+                    ""+
+                    "void main() {"+
+                    "  // The y coordinate of this sample's textures is reversed compared to"+
+                    "  // what OpenGL expects, so we invert the y coordinate."+
+                    "  gl_FragColor = texture2D(u_Texture, vec2(v_UV.x, 1.0 - v_UV.y));"+
+                    "}";
+
+    private Properties gvrProperties;
+
     private GLSurfaceView scene;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        initializeGvrView();
+
+
         setContentView(R.layout.activity_the_labyrinth);
     }
 
+    public void initializeGvrView(){
+
+        GvrView gvrView = findViewById(R.id.gvr_view);
+
+        gvrView.setRenderer(this);
+        gvrView.setTransitionViewEnabled(true);
+        gvrView.enableCardboardTriggerEmulation();  //in case the app runs with Daydream Headset
+
+        if (gvrView.setAsyncReprojectionEnabled(true)) {
+            // Async reprojection decouples the app framerate from the display framerate,
+            // allowing immersive interaction even at the throttled clockrates set by
+            // sustained performance mode.
+            AndroidCompat.setSustainedPerformanceMode(this, true);
+        }
+
+        setGvrView(gvrView);
+        gvrProperties = gvrView.getGvrApi().getCurrentProperties();
+    }
+
+    @Override
     public void onSurfaceCreated(EGLConfig config) {
+        Log.i(TAG, "onSurfaceCreated");
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        objectProgram = Util.compileProgram(OBJECT_VERTEX_SHADER_CODE, OBJECT_FRAGMENT_SHADER_CODE);
+
+        objectPositionParam = GLES20.glGetAttribLocation(objectProgram, "a_Position");
+        objectUvParam = GLES20.glGetAttribLocation(objectProgram, "a_UV");
+        objectModelViewProjectionParam = GLES20.glGetUniformLocation(objectProgram, "u_MVP");
+
+        Util.checkGlError("Object program params");
+
+
+
 
     }
 
@@ -54,6 +121,8 @@ public class TheLabyrinth extends GvrActivity implements GvrView.StereoRenderer 
     public void onFinishFrame(Viewport viewport) {}
 
     public void onRendererShutdown() {
+        Log.i(TAG, "onRendererShutdown");
+
 
     }
 }
---
+++ app/src/main/java/com/example/ellaberinto/Util.java
@@ -0,0 +1,64 @@
+package com.example.ellaberinto;
+
+import android.opengl.GLES20;
+import android.text.TextUtils;
+import android.util.Log;
+
+import static android.opengl.GLU.gluErrorString;
+
+public class Util {
+
+    private static final String TAG = "Util";
+
+    private static final boolean HALT_ON_GL_ERROR = true;
+
+    public static void checkGlError(String label) {
+        int error = GLES20.glGetError();
+        int lastError;
+        if (error != GLES20.GL_NO_ERROR) {
+            do {
+                lastError = error;
+                Log.e(TAG, label + ": glError " + gluErrorString(lastError));
+                error = GLES20.glGetError();
+            } while (error != GLES20.GL_NO_ERROR);
+
+            if (HALT_ON_GL_ERROR) {
+                throw new RuntimeException("glError " + gluErrorString(lastError));
+            }
+        }
+    }
+
+    public static int compileProgram(String vertexCode, String fragmentCode) {
+
+        checkGlError("Start of compileProgram");
+        // prepare shaders and OpenGL program
+        int vertexShader = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER);
+        GLES20.glShaderSource(vertexShader, vertexCode);
+        GLES20.glCompileShader(vertexShader);
+        checkGlError("Start of compileProgram");
+
+        int fragmentShader = GLES20.glCreateShader(GLES20.GL_FRAGMENT_SHADER);
+        GLES20.glShaderSource(fragmentShader, fragmentCode);
+        GLES20.glCompileShader(fragmentShader);
+        checkGlError("Compile fragment shader");
+
+        int program = GLES20.glCreateProgram();
+        GLES20.glAttachShader(program, vertexShader);
+        GLES20.glAttachShader(program, fragmentShader);
+
+        // Link and check for errors.
+        GLES20.glLinkProgram(program);
+        int[] linkStatus = new int[1];
+        GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS, linkStatus, 0);
+        if (linkStatus[0] != GLES20.GL_TRUE) {
+            String errorMsg = "Unable to link shader program: \n" + GLES20.glGetProgramInfoLog(program);
+            Log.e(TAG, errorMsg);
+            if (HALT_ON_GL_ERROR) {
+                throw new RuntimeException(errorMsg);
+            }
+        }
+
+        return program;
+    }
+
+}
